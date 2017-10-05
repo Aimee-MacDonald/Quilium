33c1b0589b47ca584aabeb74d9c64184bcdd2bc5
Aimee-MacDonald <admin@aimeemacdonald.com>
2017-10-06 01:29:46 +0200

    Bug Fixes

--- routes/journal.js
+++ routes/journal.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
+const Entry = require(path.join(__dirname, "../dbmodels/entry"));
+
+
 
 router.get("/entries", function(req, res, next){
   if(req.isAuthenticated()){
