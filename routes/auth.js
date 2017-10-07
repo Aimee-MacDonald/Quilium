74bb86fca7e6253c47f4d6adcb10cb5ed3c361e7
Aimee-MacDonald <admin@aimeemacdonald.com>
2017-10-07 21:00:17 +0200

    Logout

--- routes/auth.js
+++ routes/auth.js
@@ -54,4 +54,9 @@ router.post("/login", function(req, res, next){
   });
 });
 
+router.get("/logout", function(req, res, next){
+  req.logout();
+  res.redirect("/");
+});
+
 module.exports = router;
